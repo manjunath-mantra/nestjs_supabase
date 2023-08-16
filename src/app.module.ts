d2cf21852abd19102bf5814f75e048f4731483a3
Naren VR <naren.sharmendran@gmail.com>
2023-08-16 12:28:39 +0530

    Fixed import issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,12 @@ import { APIModule } from './route/api/api.module';
 import { APIRoutes } from './route/api/api.route';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
-import { AuthController } from './auth/auth.controller';
 import { SupabaseService } from './supabase/supabase.service';
 import { JwtAuthService } from './jwt/jwt.service';
 import { AuthModule } from './jwt/jwt.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
   imports: [
